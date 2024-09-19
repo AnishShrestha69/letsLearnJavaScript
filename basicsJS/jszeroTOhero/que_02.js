d35c3661d93dc74f28adf4917eb8c921c2cd3ea9
Anish Shrestha <97378475+AnishShrestha69@users.noreply.github.com>
2024-09-19 06:05:13 +0000

    config done

--- basicsJS/jszeroTOhero/que_02.js
+++ basicsJS/jszeroTOhero/que_02.js
@@ -5,7 +5,7 @@ function findLargestNumber(arr){
     }
     let largest = arr[0]
     for (let i = 1; i < arr.length; i++) {
-        if arr[i] > largest{
+        if( arr[i] > largest){
             largest = arr[i]
         }
     }
